08976b3c4a32465e6e4c8d7df1fa5e5935753543
Raphaela Barbosa <raphaelasb07@gmail.com>
2026-03-29 11:57:19 -0300

    Template

---
+++ templates/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    while(t--){
+        
+    }
+    return 0;
+}
